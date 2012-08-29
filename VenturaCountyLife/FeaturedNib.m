2966bfed4b36c0530c8441ebbefbf3279f973819
Levi Dahlstrom <locdawg18@yahoo.com>
2012-08-29 09:19:05 -0700

    first commit

---
+++ VenturaCountyLife/FeaturedNib.m
@@ -0,0 +1,44 @@
+//
+//  FeaturedNib.m
+//  VenturaCountyLife
+//
+//  Created by LeviMac on 8/29/12.
+//  Copyright (c) 2012 LeviMac. All rights reserved.
+//
+
+#import "FeaturedNib.h"
+
+@interface FeaturedNib ()
+
+@end
+
+@implementation FeaturedNib
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+    // e.g. self.myOutlet = nil;
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+@end
